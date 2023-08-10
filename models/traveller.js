2397ecc38d1aaecd9ddfbd3f099661b33f62f89f
John <ashton.basile@gmail.com>
2023-08-10 16:54:35 +0100

    I wrote the methods for the traveller class and all of the tests are passing

--- models/traveller.js
+++ models/traveller.js
@@ -4,21 +4,42 @@ class Traveller {
 	}
 
 	getJourneyStartLocations() {
+		return this.journeys.map((journey) => {
+			return journey.startLocation;
+		})
 	}
 
 	getJourneyEndLocations() {
+		return this.journeys.map((journey) => {
+			return journey.endLocation;
+		})
 	}
 
 	getJourneysByTransport(transport) {
+		return this.journeys.filter((journey) => {
+			return journey.transport === transport;
+		})
 	}
 
 	getJourneysByMinDistance(minDistance) {
+		return this.journeys.filter((journey) => {
+			return journey.distance >= minDistance;
+		})
 	}
 
 	calculateTotalDistanceTravelled() {
+		return this.journeys.reduce((total, journey) => {
+			return total + journey.distance;
+		}, 0)
 	}
-	
+
 	getUniqueModesOfTransport() {
+		return this.journeys.map((journey) => {
+			return journey.transport;
+		})
+		.filter((transport, index, array) => {
+			return array.indexOf(transport) === index;
+		});
 	}
 }
 
